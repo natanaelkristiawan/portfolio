398aa040633e892f1bb88841bb5be66a715cc30f
natanael <natanaelkristiawan@maxsol.id>
2023-02-03 17:08:14 +0800

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+
+import HomeStyle from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+    <h1 className={HomeStyle.section}>
+      Hello world!
+    </h1>
+    </>
+  )
+}
